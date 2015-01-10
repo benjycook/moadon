f0a653661e00f4d65eca949bae61c4d0595e54cd
Igor Goroshit <igor@webt.co.il>
2015-01-10 18:05:56 +0200

    start site integration

--- app/assets/javascripts/site/application.js
+++ app/assets/javascripts/site/application.js
@@ -1,3 +1,1 @@
-//= require jquery-1.10.2
-//= require handlebars-v1.3.0
-//= require ember-1.7.0
+App = Ember.Application.create({});
--- app/views/site/index.php
+++ app/views/site/index.php
@@ -4,16 +4,25 @@
 
 	<head>
 
-		<meta charset="utf-8">
-		<title>מועדונופש</title>
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>קופנופש - מועדון חברים</title>
+    
+    <meta name="description" content="">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
 		<base href='<?php echo URL::to('/'); ?>/' />
+		
 		<?php echo stylesheet_link_tag("site/application.css"); ?>
 
 	</head>
 
 	<body>
 	  
-	  <?php echo javascript_include_tag("site/application.js"); ?>
+	  <?php echo javascript_include_tag("site/manifest.js"); ?>
 
 	</body>
 
