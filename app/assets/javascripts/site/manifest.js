95499301c28f92b9c3c2fc5199126feb03d344ee
Igor Goroshit <igor@webt.co.il>
2015-01-14 15:49:43 +0200

    (site) upgrade ember & handlebars

--- app/assets/javascripts/site/manifest.js
+++ app/assets/javascripts/site/manifest.js
@@ -1,6 +1,6 @@
 //= require jquery-1.10.2
-//= require handlebars-v1.3.0
-//= require ember-1.7.0
+//= require handlebars-v2.0.0
+//= require ember-1.9.1
 //= require application.js
 //= require router.js
 //= require_tree components
