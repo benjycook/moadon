d617108301e0b5d60700c5c5749414ee14d291e3
Nick Levi <nick@webt.co.il>
2015-02-05 19:17:41 +0200

    cart fixes

--- app/controllers/Site/SiteCartController.php
+++ app/controllers/Site/SiteCartController.php
@@ -4,15 +4,13 @@ class SiteCartController extends BaseController
 {
 	protected function validateItem($item,$club,$qty,$cart)
 	{
-		if(!$item)
-			return false;
 		if(!$cartItem = CartItem::where('items_id','=',$item->id)->where('carts_id','=',$cart->id)->first())
 		{
 			$cartItem =CartItem::create(array(
 				'carts_id'=>$cart->id,
-				'items_id'=>$res->id,
-				'qty'=>$res->qty,
-				'price'=>$res->price,
+				'items_id'=>$item->id,
+				'qty'=>$qty,
+				'price'=>$item->netPrice,
 				));
 		}
 		else
@@ -20,11 +18,8 @@ class SiteCartController extends BaseController
 			$cartItem->qty = $qty;
 			$cartItem->save();
 		}
-		return = array(
-			'id'=>$item->id,
-			'qty'=>$qty,
-			'price'=>$item->netPrice,
-			);
+
+		return $cartItem->toArray();
 	}
 	public function cart($slug)
 	{
@@ -32,18 +27,22 @@ class SiteCartController extends BaseController
 		if(!$club)
 			return Response::json('מועדון זה לא נמצאה במערכת',404);
 		$data = json_decode(Request::getContent(),true);
-		if(!isset($data['cart_id'])||$cart = Cart::find($data['id']))
+		if(!isset($data['cart_id'])||!$cart = Cart::find($data['cart_id']))
 			$cart = Cart::create(array());
 		$info = array('cart_id'=>$cart->id,'items'=>array());
+		$ids = array(-1);
 		foreach ($data['items'] as $item) {
 			$origin = Item::find($item['id']);
+			if(!$origin||in_array($origin->id,$ids))
+				continue;
+			$ids[]  = $origin->id;
 			$res = $this->validateItem($origin,$club,$item['qty'],$cart);
 			if($res===false)
 				continue;
 			$info['items'][] = array(
-				'qty'=>$res->qty,
-				'id'=>$res->id,
-				'total'=>$res->qty*$res->price,
+				'qty'=>$res['qty'],
+				'id'=>$res['items_id'],
+				'total'=>$res['qty']*$res['price'],
 				);
 			
 		}
