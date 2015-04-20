f7bcdcddd0cf176a916c926b546217028c6bc42f
Igor Goroshit <igor@webt.co.il>
2015-04-20 19:18:02 +0300

    add slider to galleries

--- app/assets/javascripts/site/components/ui-slider.js
+++ app/assets/javascripts/site/components/ui-slider.js
@@ -1,47 +1,81 @@
 App.UiSliderComponent = Em.Component.extend({
+		classNames:['ui-slider'],
+		index  : -1,
+		slides : 0,
+		data: [],
 
-	classNames: 'slider',
-	
-	currentSlide: 1,
+		setupControoler: function(){
+			var lis = this.get('data.length');
+			var firstSlide = this.$('.slides ul.images li')[0].offsetWidth;
+			var totalWidth = (this.$('.slides ul.images li')[0].offsetWidth) * lis;
+			var slideWidth = this.$('.slides ul.images')[0].offsetWidth;
+			var totalSlides = Math.ceil( totalWidth / slideWidth);
 
-	slides: function(){
-		return this.images.length;
-	}.property('images'),
+			console.log(firstSlide, this.$('.slides ul.images li')[0].offsetWidth, totalWidth, totalSlides, this.$(), totalWidth / slideWidth);
 
-	links: function(){
-		var count = this.get('slides');
-		var currentSlide = this.get('currentSlide');
-		var nav = [];
-		for(var i = 1; i <= count; i++)
-		{	
-			nav.push({
-				index: i,
-				active: currentSlide == i
-			});
-		}
-		return nav;
-	}.property('slides', 'currentSlide'),
+			var properties = {
+				index : 0,
+				slides : totalSlides,
+				width: slideWidth,
+			};
 
-	// click: function(){
-	// 	if(this.get('currentSlide') >= this.get('slides'))
-	// 		this.setSlide(1);
-	// 	else
-	// 		this.setSlide(this.get('currentSlide') + 1);
-	// },
+			this.setProperties(properties);
 
-	setSlide: function(index)
-	{
-		this.set('currentSlide', index);
-		var slides = this.$('ul.slides');
-		var width = slides.find('li').first().width();
-		var right = - (width * (index - 1));
-		slides.css('right', right + 'px');
-	},
+		}.on('didInsertElement'),
 
-	actions: {
-		'setSlide': function(index){
-			this.setSlide(index);
+		computPos: function (index){
+			var newPos = index * this.get('width');
+			return -newPos;
+		},
+
+		navigation: function(){
+			var length = this.get('slides');
+			var dots = [];
+
+			for (var i = 0 ; i < length ; i++){
+				var obj = {
+					index: i ,
+					active: i === this.get('index') ,
+				};
+				dots.push(obj);
+			}
+
+			return dots;
+
+		}.property( "slides" ,"index"),
+
+		controls: function(){
+			if(this.get('slides') > 1)
+				return true;
+			return false;
+		}.property('slides'),
+
+	actions : {
+		setSlide: function(index)
+		{
+			var slides =  this.get('slides');
+
+			if( index >= slides ){
+				index = 0;
+			}
+			if(index < 0){
+				index= slides - 1;
+			}
+
+			var newPos =  this.computPos(index);
+			this.$('ul.images').css('right' , newPos + 'px');
+			this.set('index', index);
+		},
+
+		next: function() {
+			var next = this.get('index') + 1;
+			this.send('setSlide' , next);
+		},
+
+		prev: function(){
+			var prev = this.get('index') - 1;
+			this.send('setSlide' , prev);
 		}
 	}
-
 });
+
