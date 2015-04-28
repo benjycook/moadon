02bfced58afdd9a4831a9fc970fa00b898565992
nicklevi <nick@webt.co.il>
2015-04-28 09:58:21 +0300

    changed login/register flow,changed ui-from didInsertElement to conditional parlse and autocomplete

--- app/assets/javascripts/site/components/ui-form.js
+++ app/assets/javascripts/site/components/ui-form.js
@@ -1,13 +1,17 @@
 App.UiFormComponent = Em.Component.extend({
 
     tagName: 'form',
-
-    addParslay: function(){
-    		if(this.get('parslay') !== false)
-    		{
-       	 	this.$().parsley();
-        	this.$().attr('autocomplete','off');
-        }
+    autocomplete:0,
+    parsley:true,
+    addParsley: function(){
+        if(this.get('parsley'))
+            this.$().parsley();
+        if(this.get('autocomplete'))
+            this.$().attr('autocomplete','on');
+        else
+            this.$().attr('autocomplete','off');
+        if(this.$('.modal'))
+            this.$('.modal').show().addClass('in');
     }.on('didInsertElement'),
 
     submit: function()
