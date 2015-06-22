910e4b59d23fb39662ae5e294c024352d424a9df
Igor Goroshit <igor@webt.co.il>
2015-06-22 14:03:29 +0300

    add production settings

--- app/config/production/invoice.php
+++ app/config/production/invoice.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-	'url' 	=>'http://dev.webt.co.il/heshbonet/public/api/docs',
-	'key' 	=> '$2y$10$zQoqadUWd8o9Y8QzlrOSMudtKleRYhfX5gsZiiJ22Kz/M9gNWxzUe',
+	'url' 	=> 'http://heshbonet.webt.co.il/api/docs',
+	'key' 	=> '$2y$10$Q8MX41yvMMi1mBx5fPLttOOr8G8mUuRcoKzZA5HFiokNs65z6ahMS',
 );
