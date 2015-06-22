2e0bab2f9a0b6fd22c186ac6ad3a1d4363d4340d
Igor Goroshit <igor@webt.co.il>
2015-06-22 16:34:01 +0300

    fix production email address

--- app/config/production/mail.php
+++ app/config/production/mail.php
@@ -5,7 +5,7 @@ return array(
 	'driver' => 'smtp',
 	'host' => 'email-smtp.eu-west-1.amazonaws.com',
 	'port' => 587,
-	'from' => array('address' => "info@cpnclub", 'name' => "קופונופש - מועדון בחרים"),
+	'from' => array('address' => "info@cpnclub.co.il", 'name' => "קופונופש - מועדון בחרים"),
 	'encryption' => 'tls',
 	'username' => "AKIAJTKPTHGN3VMZSL2Q",
 	'password' => "AsRPiEEMQJfjJEqaosNmzDzWNzdmz16ugKwKm53/MBWy",
