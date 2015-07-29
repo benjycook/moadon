f6106c357a2002292f887442f12afdb124b0807b
nicklevi <nick@webt.co.il>
2015-07-29 19:15:57 +0300

    added totals row to reports

--- app/assets/javascripts/controllers/suppliersReport/report.js
+++ app/assets/javascripts/controllers/suppliersReport/report.js
@@ -2,31 +2,73 @@ App.ReportController = Em.ObjectController.extend({
 	displayRealizedPayedTotal:function()
     {
     	var realizedPayedTotal = this.get('realizedPayedTotal');
-    	var priceSingleRealizedTotal = this.get('priceSingleRealizedTotal');
-    	return  realizedPayedTotal+" ("+priceSingleRealizedTotal+")";
+    	if(realizedPayedTotal)
+        {
+            var priceSingleRealizedTotal = this.get('priceSingleRealizedTotal');
+            return  realizedPayedTotal+" ("+priceSingleRealizedTotal+")";
+        }
+        return "0 (0)";
     }.property('realizedPayedTotal'),
     displayOrdersPayedTotal:function()
     {
     	var ordersPayedTotal = this.get('ordersPayedTotal');
-    	var priceSingleTotal = this.get('priceSingleTotal');
-    	return  ordersPayedTotal+" ("+priceSingleTotal+")";
+    	if(ordersPayedTotal)
+        {
+            var priceSingleTotal = this.get('priceSingleTotal');
+            return  ordersPayedTotal+" ("+priceSingleTotal+")";
+        }
+        return "0 (0)";
     }.property('ordersPayedTotal'),
      displayRealizations:function()
     {
         var realizations = this.get('realizations');
-        var realizedNum = this.get('realizedNum');
-        return  realizations+" ("+realizedNum+")";
+        if(realizations)
+        {
+            var realizedNum = this.get('realizedNum');
+            return  realizations+" ("+realizedNum+")";
+        }
+        return "0 (0)";
     }.property('realizations'),
      displayRealizedPayedTotal:function()
     {
         var realizedPayedTotal = this.get('realizedPayedTotal');
-        var priceSingleRealizedTotal = this.get('priceSingleRealizedTotal');
-        return  realizedPayedTotal+" ("+priceSingleRealizedTotal+")";
+        if(realizedPayedTotal)
+        {
+          var priceSingleRealizedTotal = this.get('priceSingleRealizedTotal');
+          return  realizedPayedTotal+" ("+priceSingleRealizedTotal+")";
+        }
+        return "0 (0)";
     }.property('realizedPayedTotal'),
      displayCanceledTotal:function()
     {
         var ordersCanceledTotal = this.get('ordersCanceledTotal');
-        var ordersCanceledTotalSingle = this.get('ordersCanceledTotalSingle');
-        return  ordersCanceledTotal+" ("+ordersCanceledTotalSingle+")";
+        if(ordersCanceledTotal)
+        {
+            var ordersCanceledTotalSingle = this.get('ordersCanceledTotalSingle');
+            return  ordersCanceledTotal+" ("+ordersCanceledTotalSingle+")";
+        }
+        return "0 (0)";
     }.property('ordersCanceledTotal'),
+
+    displayOrdersCanceled:function()
+    {
+        var ordersCanceled = this.get('ordersCanceled');
+        if(ordersCanceled)
+        {
+            var ordersCanceledQty = this.get('ordersCanceledQty');
+            return  ordersCanceled+" ("+ordersCanceledQty+")";
+        }
+        return "0 (0)";
+    }.property('ordersCanceled'),
+    displayOrdersNum:function()
+    {
+        var ordersNum = this.get('ordersNum');
+        if(ordersNum)
+        {
+            var ordersTotalQty = this.get('ordersTotalQty');
+            return  ordersNum+" ("+ordersTotalQty+")";
+        }
+        return "0 (0)";
+    }.property('ordersNum'),
+    
 });
