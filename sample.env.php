73bddfaf0d9d43cf9bee0e19883bab09983d8035
Igor Goroshit <igor@webt.co.il>
2015-10-20 13:34:36 +0000

    add env sample

---
+++ sample.env.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+	'DB_HOST'	=> 'localhost',
+	'DB_NAME'	=> 'cpnclub',
+    	'DB_USERNAME' 	=> 'cpnclub',
+    	'DB_PASSWORD' 	=> 'cpnclub!@#ZxCv9090',
+
+    	'ROOTDOMAIN'	=> 'cpnclub.co.il',
+
+   	'DEFUALT_CLUB' 	=> 'cpnclub',
+	'DEFUALT_EMAIL' => NULL,
+
+	'CREDITGUARD_TID1' 	=>	'8802848',
+	'CREDITGUARD_MID1' 	=>	11741,
+	'CREDITGUARD_USER1' 	=>	'nimtag',
+	'CREDITGUARD_PASSWORD1' =>	'8jZ@WpI6',		
+	'CREDITGUARD_URL1' 	=>	'https://cgpay5.creditguard.co.il/xpo/Relay',
+
+        'CREDITGUARD_TID2'	=>	'8802849',
+        'CREDITGUARD_MID2'	=>	11741,
+        'CREDITGUARD_USER2'	=>	'nimtag',
+        'CREDITGUARD_PASSWORD2' =>	'8jZ@WpI6',
+        'CREDITGUARD_URL2'	=>	'https://cgpay5.creditguard.co.il/xpo/Relay'
+
+);
+
