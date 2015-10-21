5aa28d8c93e360a30f77eac0828b861d9e966060
Igor Goroshit <igor@webt.co.il>
2015-10-21 19:42:30 +0300

    fix suppliers login title

--- app/views/supplier/index.php
+++ app/views/supplier/index.php
@@ -7,7 +7,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta charset="utf-8" />
 		<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
-		<title>קופונופש מועדון חברים</title>
+		<title>מועדון ווב טק - כניסה לספקים</title>
 
 		<base href='<?php echo URL::to('suppliers'); ?>/' />
 		<?php echo stylesheet_link_tag("supplier/application.css"); ?>
